6074be5743b7222a0815f96d7d4b4396e9003da6
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-11 12:44:28 -0700

    complete Stack exercise

--- stack/stack.py
+++ stack/stack.py
@@ -9,17 +9,58 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+
+   Arrays come with push() and pop() methods already, and new items are added to the end.
+   With linked lists, new items are appended or removed from the head.
 """
-class Stack:
+
+from singly_linked_list import LinkedList
+
+class StackFromArray:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def pop(self):
-        pass
+
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop()
+
+class StackFromLinkedList:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    # new items will be appended at head of existing list for easy access to the most recently added element
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def pop(self):
+
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+
+# class Stack(StackFromArray):
+class Stack(StackFromLinkedList):
+    def __init__(self):
+        super().__init__()
+
+# test = Stack()
+# test.push("one")
+# test.push("two")
+# test.push("three")
+# test.pop()
+# print(len(test))
